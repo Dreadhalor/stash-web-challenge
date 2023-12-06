062e8c79175e0a7b0622e7e91edad9fa6b274321
Dreadhalor <Scotthetrick2@yahoo.com>
2023-12-05 19:36:52 -0800

    Centered items, about to add Storybook

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <div className="flex h-full w-full flex-col border-4">
+    <div className="flex h-full w-full flex-col items-center border-4">
       <div className="text-2xl">HIII</div>
       Hi, this will be more interesting later
     </div>
