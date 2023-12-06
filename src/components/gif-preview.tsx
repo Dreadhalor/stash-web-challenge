b2997e8e494dcb037d92a3e7025c906650e00886
Dreadhalor <Scotthetrick2@yahoo.com>
2023-12-06 06:54:04 -0800

    Refactored gif-preview to be more modular

--- src/components/gif-preview.tsx
+++ src/components/gif-preview.tsx
@@ -6,43 +6,50 @@ import { FaClipboard, FaClipboardCheck } from "react-icons/fa6";
 import { FaExternalLinkAlt } from "react-icons/fa";
 import { useState } from "react";
 
-type Props = {
-  gif: IGif;
+const getImagePreview = (gif: any) => {
+  return gif.images.preview.mp4 || gif.images.original.mp4 || "";
+};
+const getDisplayName = (gif: IGif) => {
+  return (
+    gif?.user?.display_name ||
+    gif?.username ||
+    gif?.source_tld ||
+    gif?.title ||
+    ""
+  );
 };
 
-const GifPreview = ({ gif }: Props) => {
-  const [linkCopiedSuccessfully, setLinkCopiedSuccessfully] =
-    useState<boolean>(false);
-
-  const getImagePreview = (gif: any) => {
-    if (gif.images.preview.mp4) {
-      return gif.images.preview.mp4;
-    }
-    if (gif.images.original.mp4) {
-      return gif.images.original.mp4;
-    }
-    return "";
-  };
-
+const UserInfo = ({ gif }: { gif: IGif }) => {
   const avatar = gif.user?.avatar_url;
-  const getDisplayName = () => {
-    if (gif?.user && gif?.user?.display_name !== "") {
-      return gif?.user?.display_name;
-    }
-    if (gif?.username !== "") {
-      return gif?.username;
-    }
-    if (gif?.source_tld !== "") {
-      return gif?.source_tld;
-    }
-    if (gif?.title !== "") {
-      return gif?.title;
-    }
-    return "";
-  };
-  const displayName = getDisplayName();
+  const displayName = getDisplayName(gif);
   const isVerified = gif?.user?.is_verified;
 
+  return (
+    <div
+      className={cn(
+        "pointer-events-none absolute bottom-0 left-0 right-0 flex h-[60px]",
+        "bg-gradient-to-b from-transparent to-black",
+        "opacity-0 transition-all duration-200 group-hover:opacity-100",
+      )}
+    >
+      <div className="mx-3 mb-3 mt-auto flex items-center text-xs text-white">
+        {avatar && <img src={avatar} className="mr-2 h-8 w-8" />}
+        <span className="mr-1">{displayName}</span>
+        {isVerified && <MdVerified />}
+      </div>
+    </div>
+  );
+};
+
+const ActionButtons = ({
+  gif,
+  linkCopiedSuccessfully,
+  setLinkCopiedSuccessfully,
+}: {
+  gif: IGif;
+  linkCopiedSuccessfully: boolean;
+  setLinkCopiedSuccessfully: (linkCopiedSuccessfully: boolean) => void;
+}) => {
   const copyToClipboard = (gifUrl: string) => {
     navigator.clipboard
       .writeText(gifUrl)
@@ -55,6 +62,49 @@ const GifPreview = ({ gif }: Props) => {
         setLinkCopiedSuccessfully(false);
       });
   };
+
+  return (
+    <div
+      className={cn(
+        "pointer-events-none absolute inset-0 flex justify-end",
+        "opacity-0 transition-all duration-200 group-hover:opacity-100",
+      )}
+    >
+      <div className="mx-3 mb-auto mt-3 flex items-center gap-1 text-white">
+        <Button
+          variant="outline"
+          size="icon"
+          className="pointer-events-auto text-base"
+          onClick={(e) => {
+            e.stopPropagation();
+            window.open(gif.url, "_blank");
+          }}
+        >
+          <FaExternalLinkAlt />
+        </Button>
+        <Button
+          variant="outline"
+          size="icon"
+          className="pointer-events-auto text-base"
+          onClick={(e) => {
+            e.stopPropagation();
+            copyToClipboard(gif.images.original.mp4);
+          }}
+        >
+          {linkCopiedSuccessfully ? <FaClipboardCheck /> : <FaClipboard />}
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+type Props = {
+  gif: IGif;
+};
+
+const GifPreview = ({ gif }: Props) => {
+  const [linkCopiedSuccessfully, setLinkCopiedSuccessfully] =
+    useState<boolean>(false);
 
   return (
     <Dialog>
@@ -71,54 +121,12 @@ const GifPreview = ({ gif }: Props) => {
             loop
             className="w-full"
           />
-          <div
-            className={cn(
-              "pointer-events-none absolute bottom-0 left-0 right-0 flex h-[60px]",
-              "bg-gradient-to-b from-transparent to-black",
-              "opacity-0 transition-all duration-200 group-hover:opacity-100",
-            )}
-          >
-            <div className="mx-3 mb-3 mt-auto flex items-center text-xs text-white">
-              {avatar && <img src={avatar} className="mr-2 h-8 w-8" />}
-              <span className="mr-1">{displayName}</span>
-              {isVerified && <MdVerified />}
-            </div>
-          </div>
-          <div
-            className={cn(
-              "pointer-events-none absolute inset-0 flex justify-end",
-              "opacity-0 transition-all duration-200 group-hover:opacity-100",
-            )}
-          >
-            <div className="mx-3 mb-auto mt-3 flex items-center gap-1 text-white">
-              <Button
-                variant="outline"
-                size="icon"
-                className="pointer-events-auto text-base"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  window.open(gif.url, "_blank");
-                }}
-              >
-                <FaExternalLinkAlt />
-              </Button>
-              <Button
-                variant="outline"
-                size="icon"
-                className="pointer-events-auto text-base"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  copyToClipboard(gif.images.original.mp4);
-                }}
-              >
-                {linkCopiedSuccessfully ? (
-                  <FaClipboardCheck />
-                ) : (
-                  <FaClipboard />
-                )}
-              </Button>
-            </div>
-          </div>
+          <UserInfo gif={gif} />
+          <ActionButtons
+            gif={gif}
+            linkCopiedSuccessfully={linkCopiedSuccessfully}
+            setLinkCopiedSuccessfully={setLinkCopiedSuccessfully}
+          />
         </div>
       </DialogTrigger>
       <DialogContent className="max-h-screen overflow-y-auto border-0 p-0">
