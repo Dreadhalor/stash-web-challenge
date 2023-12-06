62784bb7af16f1a55168fd4e4115baa0e659e1ed
Dreadhalor <Scotthetrick2@yahoo.com>
2023-12-05 21:45:18 -0800

    Adding lots of components Checkbox Label Form, though still very sloppy Adding basic searchbar with no API hookup yet

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,22 @@
+import { useState } from "react";
 import { PhotoGrid } from "./components/photo-grid";
 import { Button, Input } from "./components/ui";
 
 const App = () => {
+  const [inputState, setInputState] = useState<string>("");
+  const [searchTerm, setSearchTerm] = useState<string>("");
   return (
     <div className="flex h-full w-full flex-col items-center gap-2 border-4">
       <h1>Stash GIFs</h1>
       Hi, this will be more interesting later
       <div className="flex gap-2">
-        <Input placeholder="Search for a gif!" />
-        <Button>Search</Button>
+        <Input
+          placeholder="Search for a gif!"
+          onChange={(term) => setInputState(term.target.value)}
+        />
+        <Button onClick={() => setSearchTerm(inputState)}>Search</Button>
       </div>
-      <div></div>
+      <div>{searchTerm}</div>
       <PhotoGrid />
     </div>
   );
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,2 +1,5 @@
 export * from "./button/button";
+export * from "./checkbox/checkbox";
 export * from "./input/input";
+export * from "./label/label";
+export * from "./form/form";
