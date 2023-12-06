885391ce70b7b0a059d5f31aa72f160080b2113c
Dreadhalor <Scotthetrick2@yahoo.com>
2023-12-06 05:09:27 -0800

    Added dialog for gif lightbox

--- src/components/gif-preview.tsx
+++ src/components/gif-preview.tsx
@@ -1,10 +1,11 @@
 import { IGif } from "@giphy/js-types";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui";
 type Props = {
   gif: IGif;
 };
 
 const GifPreview = ({ gif }: Props) => {
-  const getImageSrc = (gif: any) => {
+  const getImagePreview = (gif: any) => {
     if (gif.images.preview.mp4) {
       return gif.images.preview.mp4;
     }
@@ -15,14 +16,28 @@ const GifPreview = ({ gif }: Props) => {
   };
 
   return (
-    <video
-      src={getImageSrc(gif)}
-      playsInline
-      autoPlay
-      muted
-      loop
-      className="mb-1 w-full rounded-lg"
-    />
+    <Dialog>
+      <DialogTrigger asChild>
+        <video
+          src={getImagePreview(gif)}
+          playsInline
+          autoPlay
+          muted
+          loop
+          className="mb-1 w-full cursor-pointer rounded-lg transition-all hover:brightness-75"
+        />
+      </DialogTrigger>
+      <DialogContent className="max-h-screen overflow-y-auto border-0 p-0">
+        <video
+          src={gif.images.original.mp4}
+          playsInline
+          autoPlay
+          muted
+          loop
+          className="w-full rounded-lg"
+        />
+      </DialogContent>
+    </Dialog>
   );
 };
 
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,5 +1,6 @@
 export * from "./button/button";
 export * from "./checkbox/checkbox";
+export * from "./dialog/dialog";
 export * from "./input/input";
 export * from "./label/label";
 export * from "./form/form";
