4667df9db3e6bbbad3e0de8d46b30a31e335815b
Dreadhalor <Scotthetrick2@yahoo.com>
2024-02-18 21:41:48 -0800

    Changed gifster base to /gifster

--- src/app.tsx
+++ src/app.tsx
@@ -1,33 +1,33 @@
-import { useEffect, useState } from "react";
-import { GifGrid } from "./components/gif-grid";
-import { FaArrowTrendUp } from "react-icons/fa6";
-import { SearchBar } from "./components/search-bar";
-import { TrendingSearches } from "./components/trending-searches";
+import { useEffect, useState } from 'react';
+import { GifGrid } from './components/gif-grid';
+import { FaArrowTrendUp } from 'react-icons/fa6';
+import { SearchBar } from './components/search-bar';
+import { TrendingSearches } from './components/trending-searches';
 
 const App = () => {
-  const [inputState, setInputState] = useState<string>(""); // State for input field
-  const [searchTerm, setSearchTerm] = useState<string>(""); // State for search term
+  const [inputState, setInputState] = useState<string>(''); // State for input field
+  const [searchTerm, setSearchTerm] = useState<string>(''); // State for search term
   const [trendingSearches, setTrendingSearches] = useState<string[]>([]);
 
-  const apiKey = "GZKGwdu6xlIM0iV58yFKJOFLqj0NLXFw";
-  const urlBase = "https://api.giphy.com/v1";
+  const apiKey = 'GZKGwdu6xlIM0iV58yFKJOFLqj0NLXFw';
+  const urlBase = 'https://api.giphy.com/v1';
   const trendingSearchesUrl = `${urlBase}/trending/searches?api_key=${apiKey}`;
 
   useEffect(() => {
     fetch(trendingSearchesUrl)
       .then((response) => response.json())
       .then((json) => setTrendingSearches(json.data));
-  }, []);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   const handleSearchTermClick = (term: string) => {
     let newSearchTerm = term;
-    if (term === searchTerm) newSearchTerm = "";
+    if (term === searchTerm) newSearchTerm = '';
     setInputState(newSearchTerm); // Update input field state
     setSearchTerm(newSearchTerm); // Update search term state
   };
 
   return (
-    <div className="flex h-full w-full flex-col items-center gap-2 overflow-auto px-8 py-12 sm:px-24 md:px-32">
+    <div className='flex h-full w-full flex-col items-center gap-2 overflow-auto px-8 py-12 sm:px-24 md:px-32'>
       <h1>GIFster by Scott Hetrick</h1>
       We're not GIPHY, damn it
       <SearchBar
@@ -40,7 +40,7 @@ const App = () => {
         searchTerm={searchTerm}
         handleSearchTermClick={handleSearchTermClick}
       />
-      <div className="flex w-full flex-wrap content-start items-center gap-2 text-xl">
+      <div className='flex w-full flex-wrap content-start items-center gap-2 text-xl'>
         {searchTerm ? (
           <>Search: "{searchTerm}"</>
         ) : (
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react-swc";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: '/gifster/',
 });
